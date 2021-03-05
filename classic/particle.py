fc89a5f7f33514073bf82b24268c39f1e8ddfde3
marshall tickner <marshalltickner@gmail.com>
2021-03-05 16:32:06 -0600

    Added class params, added calcA, calcV, calcPos

--- classic/particle.py
+++ classic/particle.py
@@ -1,15 +1,17 @@
 Class Particle:
     G = 6.67430e-11
-    def __init__(self,  mass, pos, velocity):
+    def __init__(self,  mass, pos, velocity, acc, prev_pos = pos,
+     prev_vel = velocity, prev_acc = acc, resolution = 1):
         self.mass = mass #scalor
         self.pos = pos #vector
         self.velocity = velocity #vector
+        self.acc = acc #vector
 
     def momentum(self):
         """calculate the momentum of the particle"""
         return [self.mass*self.velocity[0], self.mass*self.velocity[1]]
 
-    def interact(self, other):
+    def calcForce(self, other):
         """calculate the gravitation force vector"""
         m1 = self.mass
         m2 = other.mass
@@ -29,3 +31,13 @@ Class Particle:
         diff = [self.pos[0] - other.pos[0], self.pos[1] - other.pos[1]]
         mag = calcDistance(self, other)
         return [diff[0]/mag, diff[1]/mag]
+
+    def calcA(self, other):
+        F = self.calcForce(self, other)
+        return [F[0]/self.mass, F[1]/self.mass]
+
+    def calcV(self):
+        pass
+
+    def calcPos(self):
+        pass
