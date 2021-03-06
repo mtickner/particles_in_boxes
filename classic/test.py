0afcf81d860592ae0c83282adf20df47c68bc97e
marshall tickner <marshalltickner@gmail.com>
2021-03-06 16:08:32 -0600

    testing

--- classic/test.py
+++ classic/test.py
@@ -1,21 +1,27 @@
 from particle import Particle
 import matplotlib.pyplot as plt
-r = 10
-p = Particle(5.972e24, [0, 0], [0, 0], [0, 0], resolution = r)
-q = Particle(5.97e24, [54.6e5, 0], [0, 0], [0, 0], prev_pos = [54.6e5, 0], resolution = r)
+r = 1
+p = Particle(500, [0, 0], [0.0, 0.0], [0, 0], resolution = r, prev_vel = [0.0, 0.0])
+q = Particle(500, [5, 0], [0, 0.0], [0, 0], prev_pos = [5, 0], resolution = r, prev_vel = [0, 0.0])
 
 result = "Distance: {}\n Unit Vector: {}\n Force Vector: {}\n Position: {}\n"
 dist = [q.calcDistance(p)]
+vel = [q.calcV()]
 
 nq = q.next(p)
 np = p.next(q)
 dist.append(nq.calcDistance(p))
-
-for i in range(365*1000):
+vel.append(nq.calcV())
+t = [0, 1]
+for i in range(2, 250):
     nq = nq.next(np)
     np = np.next(nq)
     dist.append(nq.calcDistance(np))
+    vel.append(nq.calcV())
+    t.append(i*r)
 
-print("plot data")
-plt.plot(dist)
+vx = [x[0] for x in vel]
+vy = [y[1] for y in vel]
+plt.plot(t, vx)
+plt.plot(t, vy)
 plt.show()
